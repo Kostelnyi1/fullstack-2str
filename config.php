17e4e1a7aa2ed3c70da8976bfe1b8cc7f7a38eac
Дмитрий К <kostelnyidv1974@mail.ru>
2022-01-11 13:33:11 +0500

    корзина

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
+define('PRODUCTS_IMAGE_PATH', '/images/products/');
 session_start();
 $user = 'root';
 $password = '';
 $pdo = new Pdo('mysql:dbname=fullstack2;host=127.0.0.1', $user, $password);
+$document_root = $_SERVER['DOCUMENT_ROOT'];
